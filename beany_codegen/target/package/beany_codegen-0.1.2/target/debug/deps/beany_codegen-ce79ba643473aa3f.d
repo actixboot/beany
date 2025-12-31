dfccad63da835e7f33335f9ddb44fb4ed2caf555
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-12-31 13:00:22 +0100

    feat: updates version

---
+++ beany_codegen/target/package/beany_codegen-0.1.2/target/debug/deps/beany_codegen-ce79ba643473aa3f.d
@@ -0,0 +1,5 @@
+/home/dejwus/Projects/rust/connector/beany_codegen/target/package/beany_codegen-0.1.2/target/debug/deps/beany_codegen-ce79ba643473aa3f.d: src/lib.rs
+
+/home/dejwus/Projects/rust/connector/beany_codegen/target/package/beany_codegen-0.1.2/target/debug/deps/libbeany_codegen-ce79ba643473aa3f.so: src/lib.rs
+
+src/lib.rs:
