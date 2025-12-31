599f1eafba416d94acfbb617535a3fd8ec0274c9
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-12-31 12:52:17 +0100

    feat: v1

---
+++ beany_codegen/target/debug/build/proc-macro2-e66d8e1ef81553c8/build_script_build-e66d8e1ef81553c8.d
@@ -0,0 +1,5 @@
+/home/dejwus/Projects/rust/connector/beany_codegen/target/debug/build/proc-macro2-e66d8e1ef81553c8/build_script_build-e66d8e1ef81553c8.d: /home/dejwus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.103/build.rs
+
+/home/dejwus/Projects/rust/connector/beany_codegen/target/debug/build/proc-macro2-e66d8e1ef81553c8/build_script_build-e66d8e1ef81553c8: /home/dejwus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.103/build.rs
+
+/home/dejwus/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.103/build.rs:
---
+++ beany_codegen/target/debug/deps/beany_codegen-f6287deb4cb5c260.d
@@ -0,0 +1,5 @@
+/home/dejwus/Projects/rust/connector/beany_codegen/target/debug/deps/beany_codegen-f6287deb4cb5c260.d: src/lib.rs
+
+/home/dejwus/Projects/rust/connector/beany_codegen/target/debug/deps/libbeany_codegen-f6287deb4cb5c260.rmeta: src/lib.rs
+
+src/lib.rs:
